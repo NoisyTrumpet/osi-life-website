7d7731ac3ffe6acd4d45203be08f177da908b937
Heather Preiss <heather.l.preiss@gmail.com>
2021-08-09 10:36:37 -0500

    cleaned up services page

--- src/components/FeaturedServices/index.js
+++ src/components/FeaturedServices/index.js
@@ -10,7 +10,82 @@ import PhotoWrapper from "SVG/PhotoWrapper";
 import React from "react";
 import { Link } from "gatsby";
 
-const FeaturedServices = ({ services, id }) => {
+const FeaturedServices = ({ services, id, path }) => {
+  if (path === "/about") {
+    return (
+      <Box
+        bg={mode(`lightGrayBG`)}
+        py={5}
+        px={10}
+        borderTopLeftRadius={0}
+        borderTopRightRadius={0}
+        mt={"auto"}
+        key={id}
+      >
+        <Container>
+          <Grid
+            gridTemplateColumns={[
+              `repeat(1,85%)`,
+              `repeat(1,70%)`,
+              `repeat(3,20%)`,
+              `repeat(3,20%)`,
+              `repeat(3,20%)`,
+            ]}
+            gridTemplateRows={[
+              `repeat(3,auto)`,
+              `repeat(3,auto)`,
+              `repeat(1,auto)`,
+              `repeat(1,auto)`,
+              `repeat(1,auto)`,
+            ]}
+            gridGap={8}
+            justifyContent={`space-around`}
+          >
+            {services.map((service) => (
+              <GridItem
+                sx={{
+                  svg: {
+                    width: "100%",
+                    maxWidth: "100%",
+                  },
+                }}
+                key={service.id}
+              >
+                <PhotoWrapper
+                  image={service.image?.gatsbyImageData.images.fallback.src}
+                  width={service.image?.gatsbyImageData.width}
+                  height={service.image?.gatsbyImageData.height}
+                  imgAlt={service.image?.title}
+                  id={service.image?.id}
+                  fillColor={
+                    (service.title === "Chronic Care Management" &&
+                      "#00ADBC") ||
+                    "#FFA500"
+                  }
+                />
+                <Link
+                  to={`/${service.page.slug}`}
+                  alt={service.title}
+                  aria-label={service.title}
+                >
+                  <Heading
+                    as={`h3`}
+                    textAlign={`center`}
+                    color={mode(`primary`)}
+                    _hover={{
+                      color: mode(`secondary`),
+                    }}
+                  >
+                    {service.title}
+                  </Heading>
+                </Link>
+              </GridItem>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
+    );
+  }
   return (
     <Box
       bg={mode(`lightGrayBG`)}
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Grid,
   GridItem,
   IconButton,
@@ -17,6 +18,62 @@ const Footer = ({ path }) => {
     siteSocialAccounts: socials,
   } = contentfulSiteSettings;
 
+  if (
+    path === "/faq" ||
+    path === "/" ||
+    path === "/services" ||
+    path === "/about"
+  ) {
+    return (
+      <Box bg="lightGrayBG">
+        <Grid
+          gridTemplateColumns={[`100%`, `100%`, `100%`, `85% 15%`, `90% 10%`]}
+          gridTemplateRows={[
+            `repeat(2,.3fr)`,
+            `repeat(2,.3fr)`,
+            `repeat(2,.3fr)`,
+            `repeat(1,1fr)`,
+            `repeat(1,1fr)`,
+          ]}
+          ml={10}
+          bg={mode(`primary`)}
+          py={5}
+          px={10}
+          borderTopLeftRadius={40}
+          mt={"auto"}
+        >
+          <GridItem color={`white`}>
+            <RichText content={copyRight} />
+          </GridItem>
+          <GridItem
+            alignSelf={[`center`, `center`, `center`, `start`, `start`]}
+            justifySelf={`center`}
+          >
+            {socials.map((social) => (
+              <IconButton
+                mx={1}
+                bg={`secondary`}
+                borderTopRightRadius={`50%`}
+                as="a"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={social.url}
+                aria-label={social.title}
+                key={social.title}
+                icon={
+                  social.title === `Facebook` ? (
+                    <FaFacebookF fontSize="20px" color="white" />
+                  ) : (
+                    <FaLinkedinIn fontSize="20px" color="white" />
+                  )
+                }
+              />
+            ))}
+          </GridItem>
+        </Grid>
+      </Box>
+    );
+  }
   return (
     <Grid
       gridTemplateColumns={[`100%`, `100%`, `100%`, `85% 15%`, `90% 10%`]}
