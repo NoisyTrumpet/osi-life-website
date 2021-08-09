2fb115bba112fc26b9fccd58c2de9b41d71c632f
Cesar Benavides <cesarbenavides@icloud.com>
2021-08-09 15:53:06 -0500

    base form with chakra and formspree

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,6 +1,208 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+import {
+  Text,
+  Container,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Textarea,
+  Grid,
+} from "@chakra-ui/react";
 
 const ContactForm = ({ title, subtitle }) => {
-  // Contact Form using Charkra UI:
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (values) => {
+    const formattedValues = {
+      Name: `${values.fname} ${values.lname}`,
+      Email: values.email,
+      Message: values.message,
+      Phone: values.phone,
+    };
+    return new Promise((resolve) => {
+      axios
+        .post("https://formspree.io/f/xnqoknvr", formattedValues)
+        .then(() => {
+          alert(`Thanks for your message! We'll get back to you shortly.`);
+          resolve();
+        })
+        .catch((err) => {
+          alert(`There was an error sending your message.`, err);
+          resolve();
+        });
+    });
+  };
+
+  const Label = ({ children }) => {
+    return (
+      <FormLabel
+        position="absolute"
+        top="-2.5"
+        left="4"
+        zIndex="3"
+        color="white"
+        bg="secondary"
+        px={2}
+        borderRadius="4px"
+      >
+        {children}
+      </FormLabel>
+    );
+  };
+
+  return (
+    <Box mb={8}>
+      <Container>
+        <Box textAlign="center" color="white">
+          {title && (
+            <Text fontSize={[`xl`, `2xl`, `3xl`]} fontWeight="bold">
+              {title}
+            </Text>
+          )}
+          {subtitle && <Text>{subtitle}</Text>}
+        </Box>
+        <Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid templateColumns={`repeat(2, 1fr)`} gap={2}>
+              <FormControl my={4}>
+                <Box position="relative">
+                  <Label>First Name</Label>
+                  <Input
+                    id="fname"
+                    type="text"
+                    pt={8}
+                    pb={6}
+                    bg="white"
+                    {...register("fname", {
+                      required: "This is required",
+                      minLength: {
+                        value: 4,
+                        message: "Minimum length should be 4",
+                      },
+                    })}
+                  />
+                </Box>
+                <FormErrorMessage>
+                  {errors.fname && errors.fname.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl my={4}>
+                <Box>
+                  <Label>Last Name</Label>
+                  <Input
+                    id="lname"
+                    type="text"
+                    pt={8}
+                    pb={6}
+                    bg="white"
+                    {...register("lname", {
+                      required: "This is required",
+                      minLength: {
+                        value: 4,
+                        message: "Minimum length should be 4",
+                      },
+                    })}
+                  />
+                </Box>
+                <FormErrorMessage>
+                  {errors.lname && errors.lname.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Grid>
+
+            <Grid templateColumns={`repeat(2, 1fr)`} gap={2}>
+              <FormControl my={4}>
+                <Box>
+                  <Label>E-Mail</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    pt={8}
+                    pb={6}
+                    bg="white"
+                    {...register("email", {
+                      required: "This is required",
+                      minLength: {
+                        value: 4,
+                        message: "Minimum length should be 4",
+                      },
+                    })}
+                  />
+                </Box>
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl my={4}>
+                <Box>
+                  <Label>Phone Number</Label>
+                  <Input
+                    id="phone"
+                    type="tel"
+                    pt={8}
+                    pb={6}
+                    bg="white"
+                    {...register("phone", {
+                      required: "This is required",
+                      minLength: {
+                        value: 4,
+                        message: "Minimum length should be 4",
+                      },
+                    })}
+                  />
+                </Box>
+                <FormErrorMessage>
+                  {errors.phone && errors.phone.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Grid>
+
+            <FormControl my={4}>
+              <Box>
+                <Label>Message</Label>
+                <Textarea
+                  id="message"
+                  type="text"
+                  pt={8}
+                  pb={6}
+                  bg="white"
+                  {...register("message", {
+                    required: "This is required",
+                    minLength: {
+                      value: 10,
+                      message: "Minimum length should be 10",
+                    },
+                  })}
+                />
+              </Box>
+              <FormErrorMessage>
+                {errors.message && errors.message.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              variant="secondary"
+            >
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
+
+export default ContactForm;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,13 @@ import {
   GridItem,
   IconButton,
   useColorModeValue as mode,
+  Box,
 } from "@chakra-ui/react";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import RichText from "components/RichText";
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
+import ContactForm from "Components/ContactForm";
 
 const Footer = ({ path }) => {
   const { contentfulSiteSettings } = useStaticQuery(footerQuery);
@@ -18,15 +20,7 @@ const Footer = ({ path }) => {
   } = contentfulSiteSettings;
 
   return (
-    <Grid
-      gridTemplateColumns={[`100%`, `100%`, `100%`, `85% 15%`, `90% 10%`]}
-      gridTemplateRows={[
-        `repeat(2,.5fr)`,
-        `repeat(2,.5fr)`,
-        `repeat(2,.5fr)`,
-        `repeat(1,1fr)`,
-        `repeat(1,1fr)`,
-      ]}
+    <Box
       ml={10}
       bg={mode(`primary`)}
       py={5}
@@ -34,35 +28,50 @@ const Footer = ({ path }) => {
       borderTopLeftRadius={40}
       mt={"auto"}
     >
-      <GridItem color={`white`}>
-        <RichText content={copyRight} />
-      </GridItem>
-      <GridItem
-        alignSelf={[`center`, `center`, `center`, `start`, `start`]}
-        justifySelf={`center`}
+      <ContactForm
+        title="Learn More"
+        subtitle="Share your information below to stay up to date with the latest at OsiLIFE."
+      />
+      <Grid
+        gridTemplateColumns={[`100%`, `100%`, `100%`, `85% 15%`, `90% 10%`]}
+        gridTemplateRows={[
+          `repeat(2,.5fr)`,
+          `repeat(2,.5fr)`,
+          `repeat(2,.5fr)`,
+          `repeat(1,1fr)`,
+          `repeat(1,1fr)`,
+        ]}
       >
-        {socials.map((social) => (
-          <IconButton
-            mx={1}
-            bg={`secondary`}
-            borderTopRightRadius={`50%`}
-            as="a"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={social.url}
-            aria-label={social.title}
-            key={social.title}
-            icon={
-              social.title === `Facebook` ? (
-                <FaFacebookF fontSize="20px" color="white" />
-              ) : (
-                <FaLinkedinIn fontSize="20px" color="white" />
-              )
-            }
-          />
-        ))}
-      </GridItem>
-    </Grid>
+        <GridItem color={`white`}>
+          <RichText content={copyRight} />
+        </GridItem>
+        <GridItem
+          alignSelf={[`center`, `center`, `center`, `start`, `start`]}
+          justifySelf={`center`}
+        >
+          {socials.map((social) => (
+            <IconButton
+              mx={1}
+              bg={`secondary`}
+              borderTopRightRadius={`50%`}
+              as="a"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={social.url}
+              aria-label={social.title}
+              key={social.title}
+              icon={
+                social.title === `Facebook` ? (
+                  <FaFacebookF fontSize="20px" color="white" />
+                ) : (
+                  <FaLinkedinIn fontSize="20px" color="white" />
+                )
+              }
+            />
+          ))}
+        </GridItem>
+      </Grid>
+    </Box>
   );
 };
 
