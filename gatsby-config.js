6810800eccd5254a97520276c5ba6bed867cd983
Cesar Benavides <cesarbenavides@icloud.com>
2021-09-02 19:22:14 -0500

    pushing live with gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,25 @@ module.exports = {
     "gatsby-plugin-loadable-components-ssr",
     `gatsby-plugin-preact`,
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.GATSBY_GTAG,
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+        routeChangeEventName: "osi-route-change",
+      },
+    },
+    {
       resolve: "gatsby-source-contentful",
       options: {
         accessToken: process.env.GATSBY_CONTENTFUL_DELIVERY_API_KEY,
