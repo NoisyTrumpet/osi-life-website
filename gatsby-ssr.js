776109b49cc35cd9c4a9c809ebd33e6b5ed8620b
Cesar Benavides <cesarbenavides@icloud.com>
2022-04-14 09:43:39 -0500

    Adding proxy

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,10 +4,22 @@ import { Partytown } from '@builder.io/partytown/react';
 // You might prefer to add these as an env vars
 const ORIGIN = 'https://www.googletagmanager.com';
 
+const resolveUrl = url => {
+    if (
+      url.hostname === 'www.google-analytics.com' ||
+      url.hostname === 'connect.facebook.net'
+    ) {
+      var proxyUrl = new URL(`https://coop-atm.mygenfcu.workers.dev/?${url.href}`)
+      // proxyUrl.searchParams.append('', )
+      return proxyUrl
+    }
+    return url
+  }
+
 export const onRenderBody = ({ setHeadComponents }) => {
   if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') return null;
   setHeadComponents([
-    <Partytown key="partytown" forward={['gtag']} />,
+    <Partytown key="partytown" forward={['gtag']} resolveUrl={resolveUrl}/>,
     <script key="google-analytics" type="text/partytown" src={`${ORIGIN}/gtag/js?id=${process.env.GATSBY_GTAG}`} />,
     <script
       key="google-analytics-config"
